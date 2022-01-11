67a2f8bfeaa99d71adfd2a51679d57ddafa3b739
Praisecodes <cmancoder@gmail.com>
2022-01-11 19:08:28 +0100

    Update PHP file

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 class Dog{
-    
+    public $breed;
+    public $name;
+    public $color;
+
+    public function __construc($b,$c){
+        $this->breed = $b;
+        $this->color = $c;
+    }
+
+    public function Display($name){
+        echo $name . "'s Dog, the " . $this->color . " " . $this->breed . ", ";
+    }
 }
+
+$Dave = new Dog("Labrado", "Brown");
+$Dave->Display("David");
 ?>
