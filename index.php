20c9db534977dec5708f7b0467ad942a78f10177
Praisecodes <cmancoder@gmail.com>
2022-01-11 16:15:36 +0100

    Updated index.php

--- index.php
+++ index.php
@@ -1,24 +1,5 @@
 <?php
-class Car{
-    public $color;
-    public $model;
-    public function __construct($c, $m){
-        $this->color = $c;
-        $this->model = $m;
-    }
-
-    public function message(){
-        return "My car is a " .$this->color. " " .$this->model. "!";
-    }
-
-    public function newMessage($name){
-        return $name. "'s car is a " .$this->color. " " .$this->model. "!";
-    }
+class Dog{
+    
 }
-
-$myCar = new Car("Black", "Volvo");
-echo $myCar->message();
-echo "<br/>";
-$jakeCar = new Car("Black", "Lexus");
-echo $jakeCar->newMessage("Jake");
 ?>
